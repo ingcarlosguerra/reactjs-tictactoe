63dfe9f14d037b77454ab5727674e6d48147256e
ingcarlosguerra <125572965+ingcarlosguerra@users.noreply.github.com>
2024-03-01 13:39:37 -0500

    ajuste panorama

--- src/ResetButton.js
+++ src/ResetButton.js
@@ -4,7 +4,7 @@ export default class ResetButton extends React.Component {
     render() {
         return (
             <div>
-                <p style={{position:'absolute',left:'33%',top:'80%'}}>
+                <p style={{position:'absolute',left:'33%',top:'46%'}}>
                     <button className="btn btn-lg btn-danger" onClick={this.props.onClick} style={{ fontSize: '70px' }}>
                         Reiniciar Juego
                     </button>
--- src/Winnerline.js
+++ src/Winnerline.js
@@ -7,49 +7,54 @@ const WinnerLine = ({ winnerLine }) => {
 
   const [start, middle, end] = winnerLine;
 
-  // Calcula la posición de la línea ganadora
+  
   const styles = {
     width: '60%',
-    height: '20px',
-    backgroundColor: '#FAB627', // Cambia el color de la línea como desees
+    height: '8px',
+    backgroundColor: '#ee1da5',  // Color de fondo
     position: 'absolute',
     zIndex: 1,
-    left:'24%'
+    left: '24%',
+    boxShadow: '0 0 10px rgba(190, 9, 127, 0.8), 0 0 20px rgba(190, 9, 127, 0.8), 0 0 40px rgba(190, 9, 127, 0.8), 0 0 60px rgba(190, 9, 127, 0.8), 0 0 80px rgba(190, 9, 127, 0.8)',  // Efecto de resplandor de neón
+    border: '2px solid rgba(190, 9, 127, 0.8)',  // Borde del mismo color que el difuminado
+    borderRadius: '4px',  // Esquinas redondas
   };
-
+  
+  
+  
   // Define las coordenadas de la línea ganadora
   if (start === 0 && middle === 1 && end === 2) {
-    styles.top = '16%';  //horizontal  1
+    styles.top = '17%';  //horizontal  1
     styles.left= '20%'; 
   } else if (start === 3 && middle === 4 && end === 5) {
-    styles.top = '46%'; // horizontal 2
+    styles.top = '47.2%'; // horizontal 2
     styles.left= '20%';
   } else if (start === 6 && middle === 7 && end === 8) {
-    styles.top = '76%'; // horizontal 3
+    styles.top = '77.5%'; // horizontal 3
     styles.left= '20%';
   } else if (start === 0 && middle === 3 && end === 6) {
     styles.width = '40%';
-    styles.left = '5.5%';  // vertical 1
-    styles.top  = '45%';
+    styles.left = '5.2%';  // vertical 1
+    styles.top  = '48%';
     styles.transform = 'rotate(90deg)';
   } else if (start === 1 && middle === 4 && end === 7) {
     styles.width = '40%';
     styles.left = '30%';  // vertical 2
-    styles.top  = '45%';
+    styles.top  = '48%';
     styles.transform = 'rotate(90deg)';
   } else if (start === 2 && middle === 5 && end === 8) {
     styles.width = '40%';
     styles.left = '55%';  
-    styles.top  = '45%'; // vertical 3
+    styles.top  = '48%'; // vertical 3
     styles.transform = 'rotate(90deg)';
   } else if (start === 0 && middle === 4 && end === 8) {
     styles.transform = 'rotate(35deg)';
     styles.left='20%';
-    styles.top ='46%';
+    styles.top ='47.5%';
   } else if (start === 2 && middle === 4 && end === 6) {
     styles.transform = 'rotate(-35deg)';
     styles.left='20%';
-    styles.top ='46%';
+    styles.top ='47.5%';
   }
 
   return <div style={styles}></div>;
